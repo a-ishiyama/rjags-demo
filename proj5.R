89bb99357287b5a08f8df1c192c092bd2a32b772
a-ishiyama <akira.j.ishiyama@gmail.com>
2022-11-30 17:45:45 +0000

    need overview

--- proj5.R
+++ proj5.R
@@ -28,7 +28,22 @@ modifier <- death$d
 
 
 #-- weekly death prediction ---------------------------------------------------
-#
+#   The 'predicted_death_week' function take male and female population vector, 
+# male and female annual death rate vector, and seasonality modifier as input
+# and returns the predicted death per week "death_count_week' vector.
+#   The function first computes the weekly death rate for male and female using
+# the provided death rate. Then, using for-loop, it computes the weekly death
+# for male and female. Summing up the male and female death count in each for-
+# loop provides the total death count of that population in that week. 
+# Remaining population of that week is obtained by taking the difference
+# between the population vector and death count of that week.
+#   At the end of each loop, the male and female population vector is updated
+# by adding up 51/52th of the remaining population in each age class and 1/52th
+# of the remaining population in the age class one below. This ageing process 
+# assumes that 1/52th of the population moves up an age class each week.
+# Another assumption made in the model, constant birth-rate, is shown by adding
+# 1/52th of the original population vector's age class 0 in in for-loop when 
+# updating the new population vector.
 
 predicted_death_week <- function(population_m, population_f, dr_m, dr_f, mod){
   
@@ -73,7 +88,13 @@ predicted_death_week <- function(population_m, population_f, dr_m, dr_f, mod){
 
 
 #-- differences of observed and predicted death from and in 2020 --------------
-#
+#   In this section, the differences between observed and predicted death from
+# and in 2020 is shown using the 'predicted_death_week' function defined above.
+#   Week 157 marks the start of 2020, and using the 'population_m_20' and 
+# 'population_f_20' variables defined above, we can obtain the predicted death
+# by week from the start of 2020.
+#  The computed difference between observed and predicted death per week is 
+# defined as excess death.
 
 death_from_2020 <- death$deaths[157:length(death$deaths)]
 # actual deaths from the start of 2020 to the end of the data
@@ -103,24 +124,29 @@ print(excess_2020_sum)
 
 
 #-- plot of observed and predicted death in 2020 ------------------------------
-#
+#   In this section, the observed death per week is plotted against the 
+# prediction, former in scatter plot and latter in a continuous curve.
 
 week <- c(1:length(excess_death_from_2020))
 # generate a week vector
+
 plot(week[1:52], death_2020, col = 'blue', xlab = "weeks", 
      ylab = "number of death", ylim = c(0,max(death_2020)), 
-     main = 'plot of number of death against week')
+     main = 'excess death in 2020: 58611.92, total from 2020: 91367.86')
 # plot the observed deaths against week
 # lower limit on the y axis scale set to 0
+
 lines(week[1:52], p_death_2020, col = 'red')
 # overlay a continuous curve of the predicted deaths
+
 legend(x = 'topright', legend=c("observed", "predicted"),
-       col=c("blue", "red"), inset = 0.05, lty = 3:1 ,cex=1.2, border = "black",
-       box.lty=0, bg = rgb(1, 0, 0, alpha = 0.15))
+       col=c("blue", "red"), pch = c(1,NA), lty = c(NA,1) ,cex=1.2, 
+       border = "black")
 
 
 #-- plot of cumulative excess death from 2020 ---------------------------------
-#
+#   In this section, cumulative excess death per week from 2020 is plotted in a 
+# bar plot
 
 cum_excess <- cumsum(excess_death_from_2020)
 # compute the cumulative excess deaths by week using using the 'cumsum' 
@@ -130,7 +156,17 @@ barplot(cum_excess, ylab = "cumulative excess death",
 
 
 #-- time series modelling of excess death with jags ---------------------------
-#
+#   In this section, excess death is modeled and sampled using Bayesian method
+# implemented with the 'rjags' package.
+#   The time-series model is written in a separate text-file using BUGS script
+# and to be called upon when running the 'jags.model' function. 10000 samples
+# are taken by running 10000 iterations. Specific posterior parameters, 
+# 'k (the degree of freedom)', 'mu (mean)', 'rho(coefficient)', are monitored 
+# and returned.
+#   Sampling is done using the 'coda.samples' function of the jags package.
+#   Due to various recording problems that could occur during some dates of the
+# year (e.g. Christmas), the excess deaths on these dates are replaced as 'NA'
+# prior to running the modelling.
 
 delete <- c(51, 52, 53, 105, 106)
 excess_death_from_2020_n <- excess_death_from_2020
@@ -152,30 +188,38 @@ sample_coda <- coda.samples(model_jags, jags_params, n.iter = 10000)
 
 
 #-- trace plots and histograms of 'rho' ---------------------------------------
-#
+#   In this section, the 'rho' parameter of the model is plotted using trace 
+# plot and histogram. The 'rho' parameter is extracted out from the 
+# 'sample_coda' list estimated above.
 
 rho_list <- sample_coda[, length(excess_death_from_2020)+2]
 # extract the rho values from the sample list
-traceplot(rho_list, main = 'traceplot of rho')
+traceplot(rho_list, main = 'traceplot of rho', ylab = "rho")
 hist(unlist(rho_list), main = 'occurence of specific rho values', xlab = "rho")
 
 
 #-- computation of posterior expectation for mu -------------------------------
-#
+#   In this section, the posterior expectation of the 'mu' parameter vector is 
+# computed by taking the average of the 10000 iterations of each of the 'mu'
+# obtained from the 'sample_coda' list above.
 
-mu_coda <- rep(0,length(excess_death_from_2020)+2)
+post_mu <- rep(0,length(excess_death_from_2020)+2)
 for (i in 1:(length(excess_death_from_2020)+2)){
-  mu_coda[i]<-sum(unlist(sample_coda[,i]))/10000
+  post_mu[i]<-sum(unlist(sample_coda[,i]))/10000
 }
 # compute the average of every parameter from the jags model sampling based on 
 # the 10000 iteration
+# this is the expectation
 
-mu_coda <- mu_coda[2:(length(excess_death_from_2020)+1)]
+post_mu <- post_mu[2:(length(excess_death_from_2020)+1)]
 # limit the scope to 'mu'
 
 
 #-- plot of observed and estimated excess death using jags --------------------
-#
+#   In this section, every 50th sampled 'mu' vector is plotted as a grey curve, 
+# along with the estimated expectation for 'mu' overlaid in blue. The actual
+# observed excess death are plotted as black scatter dots, and values replaced 
+# as NA earlier are plotted as red scatter dots.
 
 p_vector <- (sample_coda[,2:(length(excess_death_from_2020)+1)])
 # extract the 'mu' vector from the jags model sampling
@@ -189,10 +233,10 @@ index <- seq(100,10000,50)
 for(i in index){
   lines(week, unlist(p_vector[i,]), type="l", col = "grey")
 }
-# overlay the remaining 199 curves using the 'mu' value obtained from 
-# 100th-10000th iterations
+# overlay the remaining 199 curves using the 'mu' value obtained from every
+# 50th of the 100th-10000th iterations
 
-lines(week, mu_coda, type="l", col = "blue")
+lines(week, post_mu, type="l", col = "blue")
 # overlay the estimated expectation of 'mu' in blue
 
 points(week, excess_death_from_2020, pch = 20)
@@ -201,12 +245,24 @@ points(week, excess_death_from_2020, pch = 20)
 points(week[delete], excess_death_from_2020[delete], pch = 20, col ="red" )
 # plot the observed excess deaths for weeks with recording problems
 
+legend(x = 'topright', legend=c("sample", "expectation", "observed", "NAs"),
+       col=c("grey", "blue", "black", "red"), pch = c(NA, NA, 20, 20), 
+       lty = c(1, 1, NA, NA) ,cex=1.2, border = "black")
+
 
 #-- residual computation ------------------------------------------------------
-#
+#   In this section, the residual of the excess death model is plotted against
+# week. Residuals are calculated by taking the difference between the observed
+# value and the expectation.
 
-residual_d <- excess_death_from_2020 - mu_coda
+residual_d <- excess_death_from_2020 - post_mu
 # residual is defined by the difference between the actual observation and the 
 # expectation of the excess death
+
 plot(week, residual_d, xlab = 'week', ylab = 'residual', 
      main = 'plot of residual against week')
+# plot the residual against week
+
+legend(x = 'topright', legend=c("residual"), pch = c(1,NA) ,cex=1.2, 
+       border = "black")
+
